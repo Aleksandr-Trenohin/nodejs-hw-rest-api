f83e8b803972f775250ba1efec118b40c9bd3f6a
Aleksandr-Trenohin <aleksandr_trenohin@bigmir.net>
2022-09-16 21:55:57 +0300

    add verified sender

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,17 +1,17 @@
 const sgMail = require("@sendgrid/mail");
 require("dotenv").config();
 
-const { SENDGRID_API_KEY } = process.env;
+const { SENDGRID_API_KEY, VERIFIED_SENDER } = process.env;
 
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 const sendEmail = async (data) => {
+  const email = { ...data, from: VERIFIED_SENDER };
   try {
-    const email = { ...data, from: "alexandrtrenohin@gmail.com" };
     await sgMail.send(email);
     return true;
   } catch (error) {
-    throw error;
+    throw new Error();
   }
 };
 
